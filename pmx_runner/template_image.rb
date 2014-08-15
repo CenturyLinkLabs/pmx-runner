8e470a01575d815e9db988fae3479eeb96c90c5b
Mike Arnold <dharmamike@gmail.com>
2014-08-15 17:42:16 -0500

    adding description and sanitizing the image name

--- pmx_runner/template_image.rb
+++ pmx_runner/template_image.rb
@@ -4,14 +4,22 @@ module PmxRunner
     attr_reader :name, :links
 
     def initialize(options={})
-      @name = options['name']
+      @name = sanitize_name(options['name'])
       @source = options['source']
+      @description = options['description']
       @ports = options['ports'] || []
       @environment = options['environment'] || []
       @command = options['command']
       @volumes = options['volumes'] || []
       @links = options['links'] || []
       @expose = options['expose'] || []
+    end
+
+    private
+
+    def sanitize_name(bad_name)
+      # Allow only chars - A-z, 0-9, ., -, _ in names
+      bad_name.gsub(/[^0-9A-z.-]|[\^]|[\`]|[\[]|[\]]/, '_')
     end
 
   end
